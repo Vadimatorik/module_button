c9a87393036d6e8e6ffbe2f5e94823c350087b29
Vadimatorik <deryabkinda@mail.ru>
2017-08-11 00:26:55 +0700

    Привожу в порядо старый код.

--- buttons_through_shift_register_one_in/buttons_through_shift_register_one_in.cpp
+++ buttons_through_shift_register_one_in/buttons_through_shift_register_one_in.cpp
@@ -1,74 +1,96 @@
 #include "buttons_through_shift_register_one_in.h"
+#include <string.h>
 
 buttons_through_shift_register_one_in::buttons_through_shift_register_one_in ( buttons_through_shift_register_one_in_cfg* cfg ) : cfg( cfg ) {
+    USER_OS_STATIC_TASK_CREATE( this->task, "b_sr", BUTTONS_THROUGH_SHIFT_REGISTER_ONE_IN_TASK_STACK_SIZE, ( void* )this, this->cfg->prio, this->task_stack, &this->task_struct );
+}
+
+// Выбрать нужную кнопку через сдвиговом регистре (подать на ее вывод 0, а на остальные 1).
+void buttons_through_shift_register_one_in::select_button ( const uint32_t &b_number ) {        // Значение b_number можно не проверять, т.к. это внутренний метод и на уровне выше уже проверено.
+    memset( this->cfg->p_button_array, 0, this->cfg->sr_buffer_byte_count );                    // Не опрашиваемые в данный момент кнопки == 0.
+    this->cfg->p_button_array[ this->cfg->p_pin_conf_array[ b_number ].byte ] |=
+        1 << this->cfg->p_pin_conf_array[ b_number ].bit;                                       // Ставим 1 на ножке (т.к. вывод подтянут к 0, то при нажатой клавише мы сразу полочим 1 на входе).
+    this->cfg->p_sr->write();                                                                   // Перезаписываем все в сдвиговом регистре.
+}
+
+// Метод возвращает true - если клавиша нажата, false, если нет.
+bool buttons_through_shift_register_one_in::check_press ( void ) {
+    return this->cfg->p_in_pin->read();
+}
+
+// Обработать нажатую клавишу.
+void buttons_through_shift_register_one_in::process_press ( const uint32_t &b_number ) {
+    // Для удобства записи.
+    sr_one_in_button_status_struct* s       = this->cfg->p_pin_conf_array->status;
+    sr_one_in_button_item_cfg*      p_st    = this->cfg->p_pin_conf_array;
+
+    if ( s->press == false ) {  // Если до этого момента кнопка была сброшена.
+        s->press        = true;                                                                 // Показываем, что нажатие произошло.
+        s->bounce       = true;                                                                 // Начинаем проверку на дребезг.
+        s->bounce_time  = p_st->ms_stabil;                                                      // Устанавливаем время для проверки дребезга контактов.
+    } else {                    // Если мы уже некоторое время держим эту кнопку.
+        if ( s->bounce == true ) {      // Если идет проверка на дребезг.
+            if ( s->bounce_time - this->cfg->delay_ms > 0 ) {   // Если время проверки еще не закончилось (мы от оставшегося времени отнимаем время, прошедшее с предыдущей проверки).
+                s->bounce_time -= this->cfg->delay_ms;
+            } else {                                            // Если время проверки закончилось и, при этом, все это время была зажата, то успех.
+                s->bounce           = false;                                                    // Проверка больше не проводится.
+                s->bounce_time      = 0;                                                        // Сбросим и время.
+                s->event_bounce     = true;                                                     // Говорим, что проверка была успешно пройдена.
+
+                // Информируем пользователя, если он этого желает (указал семафоры и очереди).
+                if ( p_st->s_press != nullptr )
+                    USER_OS_GIVE_BIN_SEMAPHORE( *p_st->s_press );
+                if ( p_st->q_press != nullptr )
+                    USER_OS_QUEUE_SEND( *q_press, &v_press, 0 );                                // Кладем в очеред без ожидания.
+            }
+        } else {                        // Если проверка на дребезг уже прошла, при этом клавишу еще держат.
+        }
+    }
+}
+   else {
+        if (button->cfg->pin_conf[button_loop].flag_dl == 1){    // Если мы отслеживаем длительное нажатие.
+            if (((uint16_t)(button->button_state[button_loop]>>8) + button->cfg->delay >= button->cfg->pin_conf[button_loop].dl_delay_ms) && // Если прошло уже больше времени, чем требуется для "долгого нажатия"
+                    ((button->button_state[button_loop] & BUTTOM_EVENT_LONG_CLICK) == 0)){// И при этом флаг раньше выставлен не был.
+                button->button_state[button_loop] |= BUTTOM_EVENT_LONG_CLICK;    // Выставляем флаг о том, что уже дождались.
+                if (button->cfg->pin_conf[button_loop].semaphore_start_long_press != NULL){    // Если по этому событию (началось длительное нажатие) требуется отдать флаг (он указан), отдаем.
+                    xSemaphoreGive(*button->cfg->pin_conf[button_loop].semaphore_start_long_press);    // Отдаем симафор.
+                };
+                if (button->cfg->pin_conf[button_loop].queue_start_long_press != NULL){    // Если есть очередь, которая ждет данных.
+                    xQueueSend( *button->cfg->pin_conf[button_loop].queue_start_long_press, &button->cfg->pin_conf[button_loop].value_start_long_press, portMAX_DELAY );
+                }
+            } else {
+                uint16_t buffer = (uint16_t)(button->button_state[button_loop]>>8);    // Сохранем прошедшее время.
+                button->button_state[button_loop] &= ~BUTTON_LONG_CLICK_TIME_MSK;    // Сбрасываем то, что было (освобождаем место.
+                button->button_state[button_loop] |= (buffer + button->cfg->delay)<<8;// Добавляем к тому, что было, время с последней проверки.
+            }
+        }
+    }
+
+
+
+// Обработать отпущенную клавишу.
+void buttons_through_shift_register_one_in::process_not_press ( const uint32_t &b_number ) {
 
 }
 
-// Маски бит в общем флаге.
-// Структура button_state для 1-й кнопки:
-// 0-й бит: Состояние кнопки в данный момент: 1 нажата, 0 - сброшена.
-// 1-й бит: Событие "прошла проверку дребезга (была нажата)": 1 - нажали, 0 - нет.
-// 2-й бит: Событие "была отпущена". 1 - была отпущена. 0 - нет.
-// 3-й бит: Событие "произошло длительное нажатие": 1 - произошло, 0 - нет.
-// 4-й бит: В данный момент идет проверка на дребезг: 1 - идет, 0 - нет.
-// 8-15: оставшееся время до окончания проверки на дребезг (в мс). А после - сколько клавиша ужа нажата: 8-24.
+void buttons_through_shift_register_one_in::task ( void* p_obj ) {
+    buttons_through_shift_register_one_in* o = ( buttons_through_shift_register_one_in* )p_obj;
+    while ( true ) {
+        for ( uint32_t b_l = 0; b_l < o->cfg->pin_count; b_l++ ) {
+            o->select_button( b_l );
+            if ( this->check_press() == true ) {
+                this->process_press( b_l );
+            } else {
 
-/*
-#define BUTTON_STATE_MSK        (1<<0)        // Маска флага состояния вывода в данный момент.
-#define BUTTON_BOUNCE_MSK        (1<<4)        // Маска Флага проверки на дребезг: 1 - идет, 0 - нет.
-#define BUTTON_EVENT_BOUNCE     (1<<1)        // Маска события "прошла проверку дребезга (была нажата)": 1 - нажали, 0 - нет.
-#define BUTTOM_EVENT_LONG_CLICK (1<<3)         // Маска события "произошло длительное нажатие": 1 - произошло, 0 - нет.
-#define BUTTON_BOUNCE_TIME_MSK     0xFF00        // Маска оставшегося времени до окончания проверки на дребезг (в мс).
-#define BUTTON_LONG_CLICK_TIME_MSK     0xFFFFFF00// Маска временисколько, сколько клавиша ужа нажата.
-*/
+            }
+        }
+        USER_OS_DELAY_MS( o->cfg->delay_ms );
+    }
+}
 
-/*
-void button_to_shift_register_task (button_to_shift_register_t *button){
-    while(1){
-        for (uint32_t button_loop = 0; button_loop<button->cfg->count_pin; button_loop++){    // Смотрим состояние всех подключенных кнопок.
-            button->cfg->point_button_array[button->cfg->pin_conf[button_loop].byte] &= ~(1<<button->cfg->pin_conf[button_loop].bit);    // Ставим 0 на ножке (т.к. вывод подтянут к 1, то при нажатой клавише мы сразу полочим 0 на входе).
-            shift_reg_write(*button->cfg->fd_shift_register, button->cfg->point_array_reg);    // Перезаписываем все в сдвиговом регистре.
-            if (port_gpio_read(button->cfg->in_pin) == 0){    // Если 0, то кнопка зажата.
-                if ((button->button_state[button_loop] & BUTTON_STATE_MSK) == 0){    // Если до этого момента кнопка была сброшена.
-                    button->button_state[button_loop] |= BUTTON_STATE_MSK|    // Показываем, что нажатие произошло.
-                            (1<<4)|    // Начинаем проверку на дребезг.
-                            (button->cfg->pin_conf[button_loop].ms_stabil<<8);    // Выставляем время для тестирования дребезга.
-                } else {    // Если мы уже некоторое время держим эту кнопку.
-                    if ((button->button_state[button_loop] & BUTTON_BOUNCE_MSK) != 0) {    // Если идет проверка на дребезг.
-                        if ((((uint8_t)(button->button_state[button_loop] >> 8)) - button->cfg->delay) > 0){    // Если время проверки еще не закончилось (мы от оставшегося времени отнимаем время, прошедшее с предыдущей проверки).
-                            uint8_t buffer = (uint8_t)(button->button_state[button_loop] >> 8);    // Сохраняем оставшееся время.
-                            button->button_state[button_loop] &= ~BUTTON_BOUNCE_TIME_MSK;        // Чистим его место.
-                            button->button_state[button_loop] |= (buffer - button->cfg->delay)<<8;    // Сохраняем оставшееся время - сколько прошло.
-                        } else { // Если время проверки закончилось и, при этом, все это время была зажата, то успех.
-                            button->button_state[button_loop] &= ~((uint16_t)(BUTTON_BOUNCE_MSK|(BUTTON_BOUNCE_TIME_MSK)));    // Проверка на дребизг завершена (за одно и сбрасываем оставшееся время, оно может быть чуть меньше, чем прошло с момента последней проверки).
-                            button->button_state[button_loop] |= BUTTON_EVENT_BOUNCE;    // Кнопка прошла проверку дребезга и теперь зажата.
-                            if (button->cfg->pin_conf[button_loop].semaphore_click != NULL){    // Если по этому событию (прошла проверку дребезга) требуется отдать флаг (он указан), отдаем.
-                                xSemaphoreGive(*button->cfg->pin_conf[button_loop].semaphore_click);    // Отдаем симафор.
-                            };
-                            if (button->cfg->pin_conf[button_loop].queue_click != NULL){    // Если есть очередь, которая ждет данных.
-                                xQueueSend( *button->cfg->pin_conf[button_loop].queue_click, &button->cfg->pin_conf[button_loop].value_click, portMAX_DELAY );
-                            }
-                        };
-                    } else { // Если проверка на дребезг уже прошла, при этом клавишу еще держат.
-                        if (button->cfg->pin_conf[button_loop].flag_dl == 1){    // Если мы отслеживаем длительное нажатие.
-                            if (((uint16_t)(button->button_state[button_loop]>>8) + button->cfg->delay >= button->cfg->pin_conf[button_loop].dl_delay_ms) && // Если прошло уже больше времени, чем требуется для "долгого нажатия"
-                                    ((button->button_state[button_loop] & BUTTOM_EVENT_LONG_CLICK) == 0)){// И при этом флаг раньше выставлен не был.
-                                button->button_state[button_loop] |= BUTTOM_EVENT_LONG_CLICK;    // Выставляем флаг о том, что уже дождались.
-                                if (button->cfg->pin_conf[button_loop].semaphore_start_long_press != NULL){    // Если по этому событию (началось длительное нажатие) требуется отдать флаг (он указан), отдаем.
-                                    xSemaphoreGive(*button->cfg->pin_conf[button_loop].semaphore_start_long_press);    // Отдаем симафор.
-                                };
-                                if (button->cfg->pin_conf[button_loop].queue_start_long_press != NULL){    // Если есть очередь, которая ждет данных.
-                                    xQueueSend( *button->cfg->pin_conf[button_loop].queue_start_long_press, &button->cfg->pin_conf[button_loop].value_start_long_press, portMAX_DELAY );
-                                }
-                            } else {
-                                uint16_t buffer = (uint16_t)(button->button_state[button_loop]>>8);    // Сохранем прошедшее время.
-                                button->button_state[button_loop] &= ~BUTTON_LONG_CLICK_TIME_MSK;    // Сбрасываем то, что было (освобождаем место.
-                                button->button_state[button_loop] |= (buffer + button->cfg->delay)<<8;// Добавляем к тому, что было, время с последней проверки.
-                            }
-                        }
-                    }
-                };
-            } else {    // Если клавишу отпустили (или была отпущена).
+
+
+// Если клавишу отпустили (или была отпущена).
                 if ((button->button_state[button_loop] & BUTTON_STATE_MSK) != 0){    // Если кнопка до этого не была отпущена.
                     if ((button->button_state[button_loop] & BUTTON_BOUNCE_MSK) != 0){    // Если кнопка проходила проверку на дребезг, но не прошла.
                         button->button_state[button_loop] = 0;    // Сбрасываем все состояния. Кнопка проверку не прошла.
@@ -95,9 +117,6 @@ void button_to_shift_register_task (button_to_shift_register_t *button){
             };
             button->cfg->point_button_array[button->cfg->pin_conf[button_loop].byte] |= 1<<button->cfg->pin_conf[button_loop].bit;    // Ставим 1 на ножке (возвращаем в исходное положение).
         };
-        vTaskDelay(button->cfg->delay);            // После просмотра состояний всех клавиш - ждем.
-    };
-};
 
 
 
--- buttons_through_shift_register_one_in/buttons_through_shift_register_one_in.h
+++ buttons_through_shift_register_one_in/buttons_through_shift_register_one_in.h
@@ -4,45 +4,59 @@
 #include "module_shift_register.h"
 #include "user_os.h"
 
+#define BUTTONS_THROUGH_SHIFT_REGISTER_ONE_IN_TASK_STACK_SIZE       100
+
+// Флаги состояния клавиши (одной).
+struct sr_one_in_button_status_struct {
+    bool        press;                  // Флаг состояния нажатия до текущей проверки (окончание предыдущей).
+    bool        bounce;                 // Флаг состояния проверки на дребезг: true - идет (еще идет), false - пройдена успешно.
+    bool        event_bounce;           // Флаг события "прошла проверку дребезга (была нажата)": true - нажали, false - нет (не прошла проверку на дребезг или была отпущена раньше, чем та завершилась).
+    bool        event_long_click;       // Флаг события "произошло длительное нажатие": true - произошло, false - нет.
+    uint8_t     bounce_time;            // Оставшееся время до окончания проверки на дребезг (в мс).
+    uint16_t    button_long_click_time; // Сколько прошло времени с момента нажатия клавиши (идет начиная с проверки дребезга).
+
+};
+
 // Структура для каждого вывода, подключенного через сдвиговый регистр.
 struct sr_one_in_button_item_cfg {
     // Положение кнопкаи в сдвиговом регистре.
-    uint8_t                             byte;                           // Бит в сдвиговом регистре, на котором весит кнопка (счет с 0).
-    uint8_t                             bit;                            // Байт, в котором находится кнопка (бит кнопки), счет с 0.
-                                                                        // Причем 0-й байт - это point_button_array.
-                                                                        // А он может быть уже 3-м или каким еще относительно point_array_reg.
+    const uint8_t                       byte;                                       // Бит в сдвиговом регистре, на котором весит кнопка (счет с 0).
+    const uint8_t                       bit;                                        // Байт, в котором находится кнопка (бит кнопки), счет с 0.
+                                                                                    // Причем 0-й байт - это point_button_array.
+                                                                                    // А он может быть уже 3-м или каким еще относительно point_array_reg.
 
     // Отслеживание дребезга контактов.
-    uint8_t                             ms_stabil;                      // Колличество мс, для стабилизации нажатия (защита от дребезга).
+    const uint8_t                             ms_stabil;                            // Колличество мс, для стабилизации нажатия (защита от дребезга).
 
     // Отслеживание разных видов нажатия.
-    bool                                flag_dl;                        // Флаг разрешения отслеживания длительного нажатия false - длительное нажатие не отслеживается, true - отслеживается.
-    uint16_t                            dl_delay_ms;                    // Время нажатия, после которого кнопка считается долго зажатой. В миллисекундах.
-    USER_OS_STATIC_BIN_SEMAPHORE*       s_click;                        // Семафор, который будет отдан сразу же после стабилизации нажатия (по клику). Если указан.
-    USER_OS_STATIC_BIN_SEMAPHORE*       s_release_click;                // Семафор, который будет отдан сразу же после отпуска кнопки (при коротком). Если указан.
-    USER_OS_STATIC_BIN_SEMAPHORE*       s_release_long_click;           // Симафор, который будет отдан сразу же, после отпускания кнопки с длительным нажатием.
-    USER_OS_STATIC_BIN_SEMAPHORE*       s_start_long_press;             // Симафор, который будет отдан сразу же, как только пройдет время, после которого кнопка будет считаться удерживаемой долго, но до ее отпускания.
-    USER_OS_STATIC_QUEUE*               q_click;                        // Очередь, в которую будет положено значение сразу же после нажатия клавиши (с учетом стабилизации от дребезга).
-    uint8_t                             v_click;                        // Значение, которое будет положено в queue_click.
-    USER_OS_STATIC_QUEUE*               q_release_click;                // Очередь, в которую будет положено значение сразу же после отпускания кнопки (короткого).
-    uint8_t                             v_release_click;
-    USER_OS_STATIC_QUEUE*               q_release_release_long_click;   // Очередь, в которую будет записано значение сразу после отпускания (длительного).
-    uint8_t                             v_release_release_long_click;
-    USER_OS_STATIC_QUEUE*               q_start_long_press;             // Очередь, в которую будет записано значение сразу же, как только пройдет время, после которого кнопка будет считаться удерживаемой долго, но до ее отпускания.
-    uint8_t                             v_start_long_press;
-} ;
+    const bool                                flag_dl;                              // Флаг разрешения отслеживания длительного нажатия false - длительное нажатие не отслеживается, true - отслеживается.
+    const uint16_t                            dl_delay_ms;                          // Время нажатия, после которого кнопка считается долго зажатой. В миллисекундах.
+          USER_OS_STATIC_BIN_SEMAPHORE*       const s_press;                        // Семафор, который будет отдан сразу же после стабилизации нажатия. Если указан.
+          USER_OS_STATIC_QUEUE*               const q_press;                        // Очередь, в которую будет положено значение сразу же после нажатия клавиши (с учетом стабилизации от дребезга).
+          const uint8_t                             v_press;                        // Значение, которое будет положено в q_press.
+
+          USER_OS_STATIC_BIN_SEMAPHORE*       const s_release_click;                // Семафор, который будет отдан сразу же после отпуска кнопки (при коротком). Если указан.
+          USER_OS_STATIC_BIN_SEMAPHORE*       const s_release_long_click;           // Симафор, который будет отдан сразу же, после отпускания кнопки с длительным нажатием.
+          USER_OS_STATIC_BIN_SEMAPHORE*       const s_start_long_press;             // Симафор, который будет отдан сразу же, как только пройдет время, после которого кнопка будет считаться удерживаемой долго, но до ее отпускания.
+          USER_OS_STATIC_QUEUE*               const q_release_click;                // Очередь, в которую будет положено значение сразу же после отпускания кнопки (короткого).
+    const uint8_t                             v_release_click;
+          USER_OS_STATIC_QUEUE*               const q_release_release_long_click;   // Очередь, в которую будет записано значение сразу после отпускания (длительного).
+    const uint8_t                             v_release_release_long_click;
+          USER_OS_STATIC_QUEUE*               const q_start_long_press;             // Очередь, в которую будет записано значение сразу же, как только пройдет время, после которого кнопка будет считаться удерживаемой долго, но до ее отпускания.
+    const uint8_t                             v_start_long_press;
+
+    sr_one_in_button_status_struct*           status;                               // Статус клавиши (используется внутренней машиной состояний).
+};
 
 struct buttons_through_shift_register_one_in_cfg {
-    pin_base*                   p_in_pin;               // Общий для всех кнопок вывод. Обязательно должен быть подтянут к +!
-    module_shift_register*      p_sr;                   // HC595 или аналог. Шина данных идет через подключенный сдвиговый регистр.
-    sr_one_in_button_item_cfg*  p_pin_conf_array;       // Указатель на массив с описанием выводов.
-    uint32_t                    pin_count;              // Колличество выводов в массиве.
-    uint32_t                    delay;                  // Пауза между опросами всех кнопок.
-    uint8_t                     prio;                   // Приоритет задачи, работающий с кнопками.
-    uint8_t*                    p_array_sr_reg;         // Указатель на первый элемент массива, который будет выводиться в сдвиговый регистр.
-                                                        // Это нужно, чтобы в случае, если, например, наши кнопки весят на 1-м 2-м регистре, то нам придется перезаписывать
-                                                        // и первый (т.к. подключены последовательно).
-    uint8_t*                    p_button_array;         // Указатель на первый байт, который используются именно кнопками.
+          pin_base*                   const p_in_pin;                               // Общий для всех кнопок вывод. Обязательно должен быть подтянут к +!
+          module_shift_register*      const p_sr;                                   // HC595 или аналог. Шина данных идет через подключенный сдвиговый регистр.
+          sr_one_in_button_item_cfg*  const p_pin_conf_array;                       // Указатель на массив с описанием выводов.
+    const uint32_t                    pin_count;                                    // Колличество выводов в массиве.
+    const uint32_t                    delay_ms;                                     // Пауза между опросами всех кнопок.
+    const uint8_t                     prio;                                         // Приоритет задачи, работающий с кнопками.
+          uint8_t*                    const p_button_array;                         // Указатель на первый байт, который используются именно кнопками.
+    const uint8_t                     sr_buffer_byte_count;                         // Количество байт, которые уходят буферу кнопок.
 };
 
 class buttons_through_shift_register_one_in {
@@ -50,7 +64,19 @@ public:
     buttons_through_shift_register_one_in ( buttons_through_shift_register_one_in_cfg* cfg );
 
     static void task ( void* p_obj );
+
 private:
+    void select_button          ( const uint32_t &b_number );                       // Выбрать нужную кнопку через сдвиговом регистре (подать на ее вывод 0, а на остальные 1).
+    bool check_press            ( void );                                           // Метод возвращает true - если клавиша нажата, false, если нет.
+
+    void process_press          ( const uint32_t &b_number );                       // Обработка клавиши в случае, если она в данный момент нажата.
+    void process_not_press      ( const uint32_t &b_number );                       // Если отпущена.
+
 
     const buttons_through_shift_register_one_in_cfg*    const cfg;
+
+    // Для создания задачи.
+    USER_OS_STATIC_STACK_TYPE           task_stack[ BUTTONS_THROUGH_SHIFT_REGISTER_ONE_IN_TASK_STACK_SIZE ] = { 0 };
+    USER_OS_STATIC_TASK_STRUCT_TYPE     task_struct = USER_OS_STATIC_TASK_STRUCT_INIT_VALUE;
+
 };
